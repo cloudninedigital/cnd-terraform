749a0b95e87853b4ae9b86a23d39eaa643bd7a22
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-07-18 10:06:01 +0200

    Extra update

--- api_cloud_run/main.tf
+++ api_cloud_run/main.tf
@@ -1,14 +1,3 @@
-data "google_project" "project" {
-  project_id = var.project
-}
-
-resource "google_secret_manager_secret_iam_member" "secret-access" {
-  secret_id = var.container_config_secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
-}
-
-
 resource "google_cloud_run_v2_service" "api_server" {
   name     = var.name
   location = var.region
