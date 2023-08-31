ab40c3e2cf2026954834d4496cdda9a0d75097e5
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-08-31 10:05:37 +0200

    change trigger to global

--- gf_gen2_bigquery_trigger_source_repo/main.tf
+++ gf_gen2_bigquery_trigger_source_repo/main.tf
@@ -136,7 +136,7 @@ resource "google_cloudfunctions2_function" "function" {
   }
 
   event_trigger {
-    trigger_region        = var.region
+    trigger_region        = "global"
     service_account_email = google_service_account.account.email
     retry_policy          = "RETRY_POLICY_RETRY"
     event_type            = "google.cloud.audit.log.v1.written"
