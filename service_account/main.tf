380a2755de70e8ebd9a09204d4e1d3102068450a
jvvbarbosaCND <joao@cloudninedigital.nl>
2023-10-04 10:06:22 +0200

    Fix service account module

--- service_account/main.tf
+++ service_account/main.tf
@@ -14,7 +14,7 @@ resource "google_service_account" "account" {
 }
 
 resource "google_project_iam_member" "roles" {
-  for_each = var.roles
+  for_each = toset(var.roles)
   project = var.project
   role    = each.key
   member  = "serviceAccount:${google_service_account.account.email}"
