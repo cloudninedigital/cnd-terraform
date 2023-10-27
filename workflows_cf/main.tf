9e9cde519f2c95589781dd067d5037c38dcb07ea
jvvbarbosaCND <joao@cloudninedigital.nl>
2023-10-27 09:10:49 +0200

    Unix format

--- workflows_cf/main.tf
+++ workflows_cf/main.tf
@@ -36,15 +36,15 @@ resource "google_service_account" "workflows_service_account" {
 
 resource "google_project_iam_member" "roles" {
   for_each = toset(local.workflows_sa_roles)
-  project = var.project
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.workflows_service_account.email}"
+  project  = var.project
+  role     = each.key
+  member   = "serviceAccount:${google_service_account.workflows_service_account.email}"
 }
 
 resource "google_project_iam_member" "token-creator-iam" {
-  project  = var.project
-  role     = "roles/iam.serviceAccountTokenCreator"
-  member    = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+  project    = var.project
+  role       = "roles/iam.serviceAccountTokenCreator"
+  member     = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
   depends_on = [google_project_service.pubsub]
 }
 
@@ -54,7 +54,7 @@ resource "google_project_iam_member" "token-creator-iam" {
 
 # Grant the eventarc.eventReceiver role to the service account
 resource "google_project_iam_member" "eventarc_receiver_binding" {
-  count = var.trigger_type == "bq" || var.trigger_type == "gcs" ? 1 : 0
+  count    = var.trigger_type == "bq" || var.trigger_type == "gcs" ? 1 : 0
   provider = google-beta
   project  = var.project
   role     = "roles/eventarc.eventReceiver"
@@ -65,34 +65,34 @@ resource "google_project_iam_member" "eventarc_receiver_binding" {
 }
 
 resource "google_project_iam_member" "cloudscheduler_admin_binding" {
-  count = var.trigger_type == "schedule" ? 1 : 0
+  count    = var.trigger_type == "schedule" ? 1 : 0
   provider = google-beta
   project  = var.project
   role     = "roles/cloudscheduler.admin"
 
-  member = "serviceAccount:${google_service_account.workflows_service_account.email}"
+  member     = "serviceAccount:${google_service_account.workflows_service_account.email}"
   depends_on = [google_service_account.workflows_service_account]
 }
 
 resource "google_project_iam_member" "gcs_binding" {
-  count = var.trigger_type == "gcs" ? 1 : 0
+  count    = var.trigger_type == "gcs" ? 1 : 0
   provider = google-beta
   project  = var.project
   role     = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.workflows_service_account.email}"
+  member   = "serviceAccount:${google_service_account.workflows_service_account.email}"
 }
 
 # END trigger specific IAM roles
 
 # Used to retrieve project_number later
 data "google_project" "project" {
-  provider = google-beta
+  provider   = google-beta
   project_id = var.project
 }
 
 # Enable Eventarc API
 resource "google_project_service" "eventarc" {
-  count = var.trigger_type == "bq" || var.trigger_type == "gcs" ? 1 : 0 
+  count              = var.trigger_type == "bq" || var.trigger_type == "gcs" ? 1 : 0
   provider           = google-beta
   service            = "eventarc.googleapis.com"
   disable_on_destroy = false
@@ -126,10 +126,10 @@ resource "google_workflows_workflow" "workflows_instance" {
   service_account = google_service_account.workflows_service_account.email
   # Imported main workflow template file
   source_contents = templatefile("modules/workflows_cf/workflow_templates/workflows_template.tftpl", {
-    project = var.project,
-    region = var.functions_region,
+    project        = var.project,
+    region         = var.functions_region,
     cloudfunctions = var.cloudfunctions,
-    trigger_type = var.trigger_type
+    trigger_type   = var.trigger_type
   })
 
   depends_on = [
@@ -145,29 +145,29 @@ resource "google_workflows_workflow" "workflows_instance" {
 # Create an Eventarc trigger routing BQ table creation/updat events to Workflows
 # Only relevant when trigger type equals 'bq'
 resource "google_eventarc_trigger" "trigger_gbq_tf" {
-  count = var.trigger_type == "bq" ? 1 : 0
+  count    = var.trigger_type == "bq" ? 1 : 0
   name     = replace("${var.name}-trigger", "_", "-")
   provider = google-beta
   location = "global"
   matching_criteria {
     attribute = "type"
-    value = "google.cloud.audit.log.v1.written"
+    value     = "google.cloud.audit.log.v1.written"
   }
 
   matching_criteria {
     attribute = "serviceName"
-    value = "bigquery.googleapis.com"
+    value     = "bigquery.googleapis.com"
   }
 
   matching_criteria {
     attribute = "methodName"
-    value = "google.cloud.bigquery.v2.JobService.InsertJob"
+    value     = "google.cloud.bigquery.v2.JobService.InsertJob"
   }
 
   matching_criteria {
     attribute = "resourceName"
-    value = "projects/${var.project}/datasets/${var.dataset}/tables/${var.table}"
-    operator = "match-path-pattern"
+    value     = "projects/${var.project}/datasets/${var.dataset}/tables/${var.table}"
+    operator  = "match-path-pattern"
   }
 
   destination {
@@ -178,7 +178,7 @@ resource "google_eventarc_trigger" "trigger_gbq_tf" {
   service_account = google_service_account.workflows_service_account.email
 
   depends_on = [google_project_service.pubsub, google_project_service.eventarc,
-  google_service_account.workflows_service_account,
+    google_service_account.workflows_service_account,
   google_project_iam_member.eventarc_receiver_binding]
 }
 
@@ -188,7 +188,7 @@ resource "google_eventarc_trigger" "trigger_gbq_tf" {
 ### START gcs TRIGGER SPECIFIC PART
 
 resource "google_storage_bucket" "workflows_trigger_bucket" {
-  count = var.trigger_type == "gcs" ? 1 : 0
+  count         = var.trigger_type == "gcs" ? 1 : 0
   name          = var.bucket_name
   location      = var.region
   storage_class = "STANDARD"
@@ -199,7 +199,7 @@ resource "google_storage_bucket" "workflows_trigger_bucket" {
 
 # Create an Eventarc trigger routing GCS events to Workflows
 resource "google_eventarc_trigger" "trigger_gcs_tf" {
-  count = var.trigger_type == "gcs" ? 1 : 0
+  count    = var.trigger_type == "gcs" ? 1 : 0
   name     = replace("${var.name}-trigger", "_", "-")
   provider = google-beta
   location = "global"
@@ -233,12 +233,12 @@ resource "google_eventarc_trigger" "trigger_gcs_tf" {
 ### START schedule TRIGGER SPECIFIC PART
 ### watch out, cloud scheduler isn't available in europe-west4 (and this won't be clear from the error logs)
 resource "google_cloud_scheduler_job" "workflow" {
-  count = var.trigger_type == "schedule" ? 1 : 0
-  project          = var.project
-  name             = "${var.name}-scheduler"
-  description      = "Cloud Scheduler for Workflow Job ${var.name}"
-  schedule         = var.schedule
-  region           = var.region
+  count       = var.trigger_type == "schedule" ? 1 : 0
+  project     = var.project
+  name        = "${var.name}-scheduler"
+  description = "Cloud Scheduler for Workflow Job ${var.name}"
+  schedule    = var.schedule
+  region      = var.region
 
   http_target {
     http_method = "POST"
@@ -256,24 +256,24 @@ resource "google_cloud_scheduler_job" "workflow" {
     }
 
   }
-  depends_on = [ google_workflows_workflow.workflows_instance, 
-  google_service_account.workflows_service_account,
-  google_project_iam_member.cloudscheduler_admin_binding,
-  google_project_service.cloudscheduler ]
+  depends_on = [google_workflows_workflow.workflows_instance,
+    google_service_account.workflows_service_account,
+    google_project_iam_member.cloudscheduler_admin_binding,
+  google_project_service.cloudscheduler]
 }
 
 
 module "bq_executor_alerting_policy" {
   source = "../bq_executor_alert_policy"
-  count = ((var.trigger_type == "bq") && var.alert_on_failure) ? 1 : 0
-  name = "${var.name}_bq_executor_alert"
+  count  = ((var.trigger_type == "bq") && var.alert_on_failure) ? 1 : 0
+  name   = "${var.name}_bq_executor_alert"
 }
 
 ## alerting policy
 module "alerting_policy" {
   source = "../alert_policy"
-  count = ((var.trigger_type != "bq") && var.alert_on_failure) ? 1 : 0
-  name = "${var.name}-alert-policy"
+  count  = ((var.trigger_type != "bq") && var.alert_on_failure) ? 1 : 0
+  name   = "${var.name}-alert-policy"
   filter = "resource.type=\"workflows.googleapis.com/Workflow\" severity=ERROR resource.labels.workflow_id=\"${var.name}\""
 }
 ### END schedule TRIGGER SPECIFIC PART
