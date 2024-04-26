87a1a2cac7b0b005009d040d38c0466a662c4e4f
Dennis van der Voorn <121879273+dennisCND@users.noreply.github.com>
2024-04-26 13:35:49 +0200

    Update main.tf

--- bq_dataset/main.tf
+++ bq_dataset/main.tf
@@ -1,6 +1,6 @@
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = var.dataset_id
-  friendly_name = var.friendly_name
+  friendly_name = "${var.friendly_name}-weirdversion"
   description = var.description
   location = var.region
 
