663fd6dc1202a103977695a74e186f6054787581
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2024-10-08 07:51:58 +0200

    proper outputs

--- gf_gen2_consolidation_triggers/outputs.tf
+++ gf_gen2_consolidation_triggers/outputs.tf
@@ -1,6 +1,6 @@
 output "cloud_function_name" {
   description = "The name of the Cloud Function created."
-  value       = google_cloudfunctions2_function.function.name
+  value       = var.trigger_type == "bq" ? google_cloudfunctions2_function.bq_function[0].name : var.trigger_type == "gcs" ? google_cloudfunctions2_function.gcs_function[0].name : google_cloudfunctions2_function.pubsub_function[0].name
 }
 
 output "cloud_function_url" {
