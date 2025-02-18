b7681a264d842ca5ee30e6a28371a965bb791607
Dennis van der Voorn <121879273+dennisCND@users.noreply.github.com>
2025-02-18 11:33:09 +0100

    Update main.tf

--- gf_gen2_bucket_trigger_gcs_source/main.tf
+++ gf_gen2_bucket_trigger_gcs_source/main.tf
@@ -181,3 +181,12 @@ resource "google_cloudfunctions2_function" "function" {
     google_project_iam_member.objectViewer
   ]
 }
+
+## alerting policy
+module "alerting_policy" {
+  source = "../alert_policy"
+  count = var.alert_on_failure ? 1 : 0
+  name = "${var.name}-alert-policy"
+  filter ="resource.type=\"cloud_run_revision\" severity=ERROR resource.labels.service_name=\"${var.name}-function\""
+  email_addresses = var.alert_email_addresses
+}
