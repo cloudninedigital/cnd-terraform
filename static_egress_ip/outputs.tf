3cf9351723609bfe0542c8c00babacfc7810c381
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2025-07-28 11:01:45 +0200

    fix output

--- static_egress_ip/outputs.tf
+++ static_egress_ip/outputs.tf
@@ -2,6 +2,7 @@ output "vpc_connector" {
   value = google_vpc_access_connector.connector.self_link
 }
 
-output "static_ip" {
-  value = google_compute_address.address.address
+output "static_ips" {
+  description = "List of all static IP addresses used for NAT."
+  value       = [for ip in google_compute_address.nat_ips : ip.address]
 }
