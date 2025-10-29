36b75be44aaccf558e712dd607d28574d2a5c01b
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2025-10-29 08:40:30 +0100

    fix the all users iam member

--- gf_gen2_consolidation_triggers/main.tf
+++ gf_gen2_consolidation_triggers/main.tf
@@ -460,6 +460,8 @@ resource "google_cloudfunctions2_function" "http_function" {
 resource "google_cloudfunctions2_function_iam_member" "public_invoker" {
   count = var.make_http_endpoint_public && var.trigger_type == "http" ? 1 : 0
   cloud_function = google_cloudfunctions2_function.http_function[0].name
+  project = google_cloudfunctions2_function.http_function[0].project
+  location = google_cloudfunctions2_function.http_function[0].location
   role     = "roles/cloudfunctions.invoker"
   member   = "allUsers"  # This grants public access
 }
